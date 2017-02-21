d49b7ec1888c027532de7130dd84214b3a648d8c
Hawk94 <x99tom.miller@gmail.com>
2017-02-21 22:02:53 +0000

    New project

--- portfolios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from portfolios import views
-
-urlpatterns = [
-  url(r'^$', views.index, name='index'),
-  url(r'^test/$', views.test, name='test'),
-  url(r'^profile/$', views.profile, name='profile'),
-]
